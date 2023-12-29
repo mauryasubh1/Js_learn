6a9c6f2b81372ad7cfe4f90a116649e20733698c
mauryasubh1 <155166983+mauryasubh1@users.noreply.github.com>
2023-12-29 13:35:41 +0000

    container config created

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+console.log("here we go !")
+
+
+const  myDoc = "constant value"
+
+let v1 = 12345
+
+var v2 = "js learn"
+
+v3 = 123455
+
+v4 = "sab chalta hai"
+
+// myDoc = "we try to change to given value"
+
+console.log(myDoc);
+
+console.table([myDoc,v1,v2, v3, v4 ])  // using table we van print multiple values
