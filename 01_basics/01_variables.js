e74017a1ca3ac80521314756a2e6d4780e296b0a
mauryasubh1 <155166983+mauryasubh1@users.noreply.github.com>
2023-12-29 15:10:11 +0000

    data types

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,8 +11,19 @@ v3 = 123455
 
 v4 = "sab chalta hai"
 
+let statecode;    // if value was not assing means its undefine 
+
 // myDoc = "we try to change to given value"
 
 console.log(myDoc);
 
-console.table([myDoc,v1,v2, v3, v4 ])  // using table we van print multiple values
+console.table([myDoc,v1,v2, v3, v4, statecode ])  // using table we van print multiple values
+
+
+
+
+/*
+
+Note :. Do not use the "Var"  , Because its create the issue in block scope and functional scope.
+
+*/
---
+++ 01_basics/02_data_type.js
@@ -0,0 +1,19 @@
+console.log("========================learning data type=================================");
+
+
+
+// number =>  2 to power 53
+// String => 
+// Boolean => true/false
+// BigInt
+
+// undefined =>
+// Symbol =>  unique
+
+
+// object 
+
+// null  => standalone value   and null is also object  
+console.log(typeof null);
+console.log(typeof undefined);
+
